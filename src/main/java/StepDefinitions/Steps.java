1079e45feaed3c14e3b84a615b23428791ccc45c
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-06-09 09:17:22 +0300

    StepDefinitions added

---
+++ src/main/java/StepDefinitions/Steps.java
@@ -0,0 +1,10 @@
+package StepDefinitions;
+
+import cucumber.api.java.en.*;
+
+public class Steps {
+    @Given("Open selenium webdriver")
+    public void I_have_cukes_in_my_belly() {
+        System.out.println("Test");
+    }
+}
