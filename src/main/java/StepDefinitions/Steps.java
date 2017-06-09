0da18094cf92f8424dcb5882356d9f36ec07f6d8
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-06-09 10:32:54 +0300

    "WEB Open the game collection of a user." scenario added

--- src/main/java/StepDefinitions/Steps.java
+++ src/main/java/StepDefinitions/Steps.java
@@ -1,10 +1,48 @@
 package StepDefinitions;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Steps {
+
+    static WebDriver driver;
+
     @Given("Open selenium webdriver")
-    public void I_have_cukes_in_my_belly() {
-        System.out.println("Test");
+    public void OpenDriver() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Given("Open page '(.*)'")
+    public void OpenPage(String url) {
+        driver.get(url);
+    }
+
+    @When("Click element '(.*)'")
+    public void ClickElement(String css){
+        driver.findElement(By.cssSelector(css)).click();
+    }
+
+    @When("Fill textbox '(.*)' as '(.*)'")
+    public void FillTextbox(String css,String text){
+        driver.findElement(By.cssSelector(css)).clear();
+        driver.findElement(By.cssSelector(css)).sendKeys(text);
+    }
+
+    @Then("I see element '(.*)' text is '(.*)'")
+    public void CheckText(String css,String expected) throws InterruptedException {
+        Thread.sleep(1000);
+        String actual=driver.findElement(By.cssSelector(css)).getText();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @After
+    public void CloseDriver() {
+        driver.close();
     }
 }
--- src/test/resources/TestCases.feature
+++ src/test/resources/TestCases.feature
@@ -1,4 +1,10 @@
 Feature: Assessment
 
   Scenario: Open page
+    #WEB Open the game collection of a user.
     Given Open selenium webdriver
+    When  Open page 'https://www.boardgamegeek.com'
+    And   Fill textbox '#login_username' as 'celikgumusdag'
+    And   Fill textbox '#login_password' as '12345678'
+    And   Click element '.loginbox>input'
+    Then  I see element '.mt5>a' text is 'celikgumusdag'
