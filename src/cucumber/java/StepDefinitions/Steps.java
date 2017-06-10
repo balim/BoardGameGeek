7a4d8485205c42cdfda1764ced771fb994ec2e0c
Çelik <celikgumusdag@gmail.com>
2017-06-10 12:57:55 +0300

    relocate for cucumber standarts

--- src/cucumber/java/StepDefinitions/Steps.java
+++ src/cucumber/java/StepDefinitions/Steps.java
@@ -7,12 +7,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class Steps {
 
     static WebDriver driver;
 
-    @Given("Open selenium webdriver")
+    @Given("^Open selenium webdriver$")
     public void OpenDriver() {
         System.out.print(System.getProperty("user.dir"));
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
@@ -20,31 +19,32 @@ public class Steps {
         driver.manage().window().maximize();
     }
 
-    @Given("Open page '(.*)'")
+    @When("^Open page '(.*)'$")
     public void OpenPage(String url) {
         driver.get(url);
     }
 
-    @When("Click element '(.*)'")
-    public void ClickElement(String css){
+    @When("^Click element '(.*)'$")
+    public void ClickElement(String css) {
         driver.findElement(By.cssSelector(css)).click();
     }
 
-    @When("Fill textbox '(.*)' as '(.*)'")
-    public void FillTextbox(String css,String text){
+    @When("^Fill textbox '(.*)' as '(.*)'$")
+    public void FillTextbox(String css, String text) {
         driver.findElement(By.cssSelector(css)).clear();
         driver.findElement(By.cssSelector(css)).sendKeys(text);
     }
 
-    @Then("I see element '(.*)' text is '(.*)'")
-    public void CheckText(String css,String expected) throws InterruptedException {
+    @Then("^I see element '(.*)' text is '(.*)'$")
+    public void CheckText(String css, String expected) throws InterruptedException {
         Thread.sleep(1000);
-        String actual=driver.findElement(By.cssSelector(css)).getText();
-        Assert.assertEquals(expected,actual);
+        String actual = driver.findElement(By.cssSelector(css)).getText();
+        Assert.assertEquals(expected, actual);
     }
 
     @After
     public void CloseDriver() {
         driver.close();
     }
+
 }
